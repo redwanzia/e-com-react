8a5cff2ab47209ae86160562179d90df24070982
Redwan Zia <redwan.zia@gmail.com>
2020-01-17 18:50:10 -0500

    Added header component with styling

--- ecom-react/src/App.js
+++ ecom-react/src/App.js
@@ -3,12 +3,14 @@ import {Route, Switch} from 'react-router-dom'
 import HomePageComponent from './pages/homepage/component';
 import ShopPage from './pages/shopPage/ShopPage'
 import './App.css';
+import Header from './component/header/Header';
 
 
 
 function App() {
 	return (
 		<div>
+			<Header/>
 			<Switch>
 				<Route exact path="/" component={HomePageComponent}/>
 				<Route path="/shop" component={ShopPage}/>				
