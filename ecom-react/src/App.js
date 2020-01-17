642843102cb3c887bca8103265c20c4b0eabea30
Redwan Zia <redwan.zia@gmail.com>
2020-01-16 23:44:56 -0500

    Added e-com-react project with hame page structure

---
+++ ecom-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePageComponenet from './HomePageComponenet ';
+
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<HomePageComponenet />
+		</div>
+	);
+}
+
+export default App;
